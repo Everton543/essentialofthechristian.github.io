e396f00d7999eb0847ee9a3372a74d2a65522c2d
Everton543 <everton.felipe.alves@gmail.com>
2022-09-25 10:11:14 -0300

    UPDATE menu links

--- src/components/menu.jsx
+++ src/components/menu.jsx
@@ -9,8 +9,8 @@ function Menu() {
     <div className='menuContainer'>
       <div className='menuItemsContainer'>
         <img src={logo} alt="logo" width={"50px"} />
-        <MenuItem link={'/essentialofthechristian.github.io/'} title={'Home'}> </MenuItem>
-        <MenuItem link={'/essentialofthechristian.github.io//#/Jornals'} title={'Messsages'}></MenuItem>
+        <MenuItem link={'/'} title={'Home'}> </MenuItem>
+        <MenuItem link={'/#/Jornals'} title={'Messsages'}></MenuItem>
 
       </div>
     </div>
