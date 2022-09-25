6f7c583a2999554c1690c94fe58091f20e6d3796
Everton543 <everton.felipe.alves@gmail.com>
2022-09-24 21:49:57 -0300

    SERVER TEST

--- src/service/model.js
+++ src/service/model.js
@@ -37,19 +37,19 @@ export function getJornals(){
     const list = [
         {
             title: "What kind of soil do you want to be?",
-            subtilte: "Parable of the Sower",
+            subtitle: "Parable of the Sower",
             text: 'Luke 8:5-15 \"But that on the good ground are they, which in an honest and good heart, having heard the word, keep it, and bring forth fruit with patience. \" \n (Luke 8:15) \n Before being Christian I liked to watch animation videos about the bible, everything that I knew about the bible was from these videos and the few things that I listened to in the multitude of churches that my mother made me go through.\n Through all that information the one that changed my life is the Parable of the Sower. Every time someone tried to teach me about Christ I would always listen because of education. \n Still, one day when I was listening to two young men teaching about the gospel of Jesus Christ  I had the Spirit make me remember the teachings on the parable that made me an essential question \n \"What kind of soil do you want to be?\". \n  From that day on, I started to live my life with these questions in my mind, before each decision I would ask myself, what kind of soil does this show that I am? \n\n This website is for all Christians, and I would like each of you to share here what changed in your life after practicing the teachings of the Parable of the Sower.',
             id: "first"
         },
         {
             title: "Test",
-            subtilte: "Test is a test",
+            subtitle: "Test is a test",
             text: 'This is a test',
             id: "test"
         },
         {
             title: "Test2",
-            subtilte: "Test is a test2",
+            subtitle: "Test is a test2",
             text: 'This is a test2',
             id: "test2"
         }
@@ -62,19 +62,19 @@ export function getJornalById(id){
     const list = [
         {
             title: "What kind of soil do you want to be?",
-            subtilte: "Parable of the Sower",
+            subtitle: "Parable of the Sower",
             text: 'Luke 8:5-15 \"But that on the good ground are they, which in an honest and good heart, having heard the word, keep it, and bring forth fruit with patience. \" \n (Luke 8:15) \n Before being Christian I liked to watch animation videos about the bible, everything that I knew about the bible was from these videos and the few things that I listened to in the multitude of churches that my mother made me go through.\n Through all that information the one that changed my life is the Parable of the Sower. Every time someone tried to teach me about Christ I would always listen because of education. \n Still, one day when I was listening to two young men teaching about the gospel of Jesus Christ  I had the Spirit make me remember the teachings on the parable that made me an essential question \n \"What kind of soil do you want to be?\". \n  From that day on, I started to live my life with these questions in my mind, before each decision I would ask myself, what kind of soil does this show that I am? \n\n This website is for all Christians, and I would like each of you to share here what changed in your life after practicing the teachings of the Parable of the Sower.',
             id: "first"
         },
         {
             title: "Test",
-            subtilte: "Test is a test",
+            subtitle: "Test is a test",
             text: 'This is a test',
             id: "test"
         },
         {
             title: "Test2",
-            subtilte: "Test is a test2",
+            subtitle: "Test is a test2",
             text: 'This is a test2',
             id: "test2"
         }
